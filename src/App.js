4adb40e425caa56139c513909e6ff8f631452b22
sharzad <babaeishahrzad@gmail.com>
2023-03-27 14:42:36 +0200

    added github and Netlify

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import "./App.css";
 import React from "react";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        {" "}
+        This roject was coded by Shahrzad B.
+        <a href="#" target={"_blank"}>
+          Github
+        </a>
+        &
+        <a href="#" target={"_blank"}>
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-
