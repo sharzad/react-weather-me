66bc7c98635023cb8b37ca0779ec1a21fbe752a6
sharzad <babaeishahrzad@gmail.com>
2023-03-27 14:51:28 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import React from "react";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        {" "}
-        This roject was coded by Shahrzad B.
-        <a href="#" target={"_blank"}>
-          Github
-        </a>
-        &
-        <a href="#" target={"_blank"}>
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          {" "}
+          This roject was coded by Shahrzad B.
+          <a href="#" target={"_blank"}>
+            Github
+          </a>
+          &
+          <a href="#" target={"_blank"}>
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
