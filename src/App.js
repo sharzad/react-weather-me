282d7b0440196de7c1fa4ecd6296a6856db2f654
sharzad <babaeishahrzad@gmail.com>
2023-03-29 23:57:30 +0200

    add Api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import React from "react";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+       
+        <Weather />
         <footer>
           {" "}
-          This roject was coded by Shahrzad B.
+          This project was coded by Shahrzad B.
           <a href="#" target={"_blank"}>
             Github
           </a>
